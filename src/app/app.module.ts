ea6651c9f72db4f0083b5229af569dbd9075cc67
Masha <teenderrr@gmail.com>
2023-02-02 11:50:00 +0200

    password field

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { SectionsComponent } from './sections/sections.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    SectionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
